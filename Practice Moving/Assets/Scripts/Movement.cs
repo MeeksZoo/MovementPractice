4026dd08d793c1ca5edd38ff6835640a9beb56df
Jenna <jennasmeeks@gmail.com>
2018-09-26 19:39:56 -0600

    Progress

--- Practice Moving/Assets/Scripts/Movement.cs
+++ Practice Moving/Assets/Scripts/Movement.cs
@@ -1,16 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Movement : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+﻿	using UnityEngine; 
+ 
+	[RequireComponent(typeof(CharacterController))] 
+	public class CharacterMover : MonoBehaviour 
+	{ 
+		private CharacterController controller; 
+		private Vector3 position, rotation; 
+		public float Speed = 3.0f, JumpSpeed = 4.0f, Gravity = 9.81f; 
+ 
+		void Start() 
+		{ 
+			controller = GetComponent<CharacterController>(); 
+		} 
+ 
+		void Update() 
+		{ 
+			if (controller.isGrounded) 
+			{ 
+				position.Set(X.value, Y.value, Z.value); 
+				rotation.Set(Q.value, R.value, S.value); 
+				transform.Rotate(rotation); 
+				position = transform.TransformDirection(position); 
+ 
+				if (Input.GetButton("Jump")) 
+				{ 
+					position.y = JumpSpeed; 
+				} 
+			} 
+ 
+			position.y -= Gravity * Time.deltaTime; 
+         
+			controller.Move(position * Time.deltaTime); 
 }
